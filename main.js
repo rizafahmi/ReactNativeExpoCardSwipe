13f2dd58b723913ec03f4190e9286a37abab435c
Riza Fahmi <rizafahmi@gmail.com>
2017-06-24 09:27:08 +0700

    Drag the card

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import Expo from 'expo'
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { Card, Button } from 'react-native-elements'
 
 import Deck from './src/Deck'
 
@@ -49,7 +50,18 @@ const DATA = [
 
 class App extends React.Component {
   renderCard (item) {
-    return <Text>{item.text}</Text>
+    return (
+      <Card key={item.id} title={item.text} image={{ uri: item.uri }}>
+        <Text style={{ marginBottom: 10 }}>
+          I can customize!
+        </Text>
+        <Button
+          icon={{ name: 'code' }}
+          backgroundColor='#03A9F4'
+          title='View now!'
+        />
+      </Card>
+    )
   }
   render () {
     return (
--- src/Deck.js
+++ src/Deck.js
@@ -1,10 +1,21 @@
 import React from 'react'
-import { View, Animated } from 'react-native'
+import { Animated, PanResponder } from 'react-native'
 
 export default ({ data, renderCard }) => {
+  const position = new Animated.ValueXY()
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: (event, gesture) => {
+      position.setValue({
+        x: gesture.dx,
+        y: gesture.dy
+      })
+    },
+    onPanResponderRelease: () => {}
+  })
   return (
-    <View>
+    <Animated.View style={position.getLayout()} {...panResponder.panHandlers}>
       {data.map(item => renderCard(item))}
-    </View>
+    </Animated.View>
   )
 }
