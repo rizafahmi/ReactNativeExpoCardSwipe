2311d0b673c40fe02293376aa31f9fe98970b96e
Riza Fahmi <rizafahmi@gmail.com>
2017-06-24 12:10:42 +0700

    Force swipe right

--- src/Deck.js
+++ src/Deck.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { View, Animated, PanResponder, Dimensions } from 'react-native'
 
+const SCREEN_WIDTH = Dimensions.get('window').width
+const SCALE = 1.5
+const SWIPE_OUT_DURATION = 250
+
 const getCardStyle = position => {
-  const SCREEN_WIDTH = Dimensions.get('window').width
-  const SCALE = 1.5
   const rotate = position.x.interpolate({
     inputRange: [(-SCREEN_WIDTH) * SCALE, 0, SCREEN_WIDTH * SCALE],
     outputRange: ['-120deg', '0deg', '120deg']
@@ -21,8 +23,19 @@ const resetPosition = position => {
     .start()
 }
 
+const forceSwipeRight = position => {
+  Animated.timing(position, {
+    toValue: { x: SCREEN_WIDTH, y: 0 },
+    duration: SWIPE_OUT_DURATION
+  })
+    .start()
+}
+
 export default ({ data, renderCard }) => {
+  const SWIPE_WIDTH = Dimensions.get('window').width
+  const SWIPE_TRESHOLD = 0.25 * SWIPE_WIDTH
   const position = new Animated.ValueXY()
+
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
     onPanResponderMove: (event, gesture) => {
@@ -31,8 +44,14 @@ export default ({ data, renderCard }) => {
         y: gesture.dy
       })
     },
-    onPanResponderRelease: () => {
-      resetPosition(position)
+    onPanResponderRelease: (event, gesture) => {
+      if (gesture.dx > SWIPE_TRESHOLD) {
+        forceSwipeRight(position)
+      } else if (gesture.dx < -SWIPE_TRESHOLD) {
+        console.log('Swipe Left!')
+      } else {
+        resetPosition(position)
+      }
     }
   })
   return (
